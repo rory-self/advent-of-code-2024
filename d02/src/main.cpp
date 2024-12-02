198a3780a10119dc4d9c9687ad04ef025206cf8a
Rory Self <rory.self1@gmail.com>
2024-12-02 18:26:06 +1100

    Finished day 2 part 2

--- d02/src/main.cpp
+++ d02/src/main.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <fstream>
 #include <sstream>
+#include <iterator>
 
 namespace {
     auto read_reports_from_file(const std::string& file_path) -> std::vector<std::vector<int>> {
@@ -25,29 +26,75 @@ namespace {
         return reports;
     }
 
-    auto check_report_safety(const std::vector<int>& report) -> bool {
+    auto is_report_increasing(const std::vector<int>& report) -> bool {
+        const auto increasing_pair = [](const auto e1, const auto e2) { return e1 < e2; };
+        const auto first_increasing_pair = std::ranges::adjacent_find(report, increasing_pair);
+        if (first_increasing_pair == report.cend()) {
+            return false;
+        }
+
+        const auto second_increasing_pair = std::adjacent_find(first_increasing_pair + 1, report.cend(), increasing_pair);
+        return second_increasing_pair != report.cend();
+    }
+
+    auto get_unsafe_level(const std::vector<int>& report) -> std::vector<int>::const_iterator {
         if (report.size() <= 2) {
-            return true;
+            return report.cend();
         }
 
         constexpr auto min_difference = 1;
         constexpr auto max_difference = 3;
-        const auto is_increasing = *report.cbegin() < *(report.cbegin() + 1);
-        const auto are_unsafe_levels = [is_increasing, max_difference](const auto e1, const auto e2) {
+        const auto is_increasing = is_report_increasing(report);
+        const auto are_unsafe_levels = [is_increasing, min_difference, max_difference](const auto e1, const auto e2) {
             const auto difference = std::abs(e2 - e1);
             return e1 < e2 != is_increasing or difference > max_difference or difference < min_difference;
         };
 
-        return std::ranges::adjacent_find(report, are_unsafe_levels) == report.cend();
+        return std::ranges::adjacent_find(report, are_unsafe_levels);
+    }
+
+    auto damp_report(
+        const std::vector<int>& report,
+        const std::vector<int>::const_iterator level_to_remove
+    ) -> std::vector<int> {
+        std::vector<int> damped_report;
+        damped_report.reserve(report.size() - 1);
+        damped_report.insert(damped_report.end(), report.cbegin(), level_to_remove);
+        damped_report.insert(damped_report.end(), level_to_remove + 1, report.cend());
+
+        return damped_report;
+    }
+
+    auto check_damped_report_safety(const std::vector<int>& report) -> bool {
+        const auto first_unsafe_level = get_unsafe_level(report);
+        if (first_unsafe_level == report.cend()) {
+            return true;
+        }
+
+        auto damped_report = damp_report(report, first_unsafe_level);
+        if (get_unsafe_level(damped_report) == damped_report.cend()) {
+            return true;
+        }
+
+        damped_report = damp_report(report, first_unsafe_level + 1);
+        if (get_unsafe_level(damped_report) == damped_report.cend()) {
+            return true;
+        }
+        return false;
     }
 }
 
-int main() {
+auto main() -> int {
     const auto file_path = std::string{"input.txt"};
     const auto reports = read_reports_from_file(file_path);
 
-    const auto num_safe_reports = std::ranges::count_if(reports, check_report_safety);
+    const auto num_safe_reports = std::ranges::count_if(reports, [](const auto& report) {
+        return get_unsafe_level(report) == report.cend();
+    });
+
+    const auto num_safe_damped_reports = std::ranges::count_if(reports, check_damped_report_safety);
 
     std::cout << "Number of safe reports: " << num_safe_reports << '\n';
+    std::cout << "Number of safe reports using the problem dampener: " << num_safe_damped_reports << '\n';
     return 0;
 }
