7e5bba9261d9d0393687d98ee170f1be00435209
Rory Self <rory.self1@gmail.com>
2024-12-11 16:58:04 +1100

    Fixed day 9

--- d09/src/main.cpp
+++ d09/src/main.cpp
@@ -10,23 +10,24 @@ namespace {
         std::size_t length;
     };
 
-    [[nodiscard]] auto read_blocks_from_file(const std::string& file_path) -> std::vector<Block> {
+    [[nodiscard]] auto read_disk_from_file(const std::string& file_path) -> std::vector<Block> {
         auto file = std::ifstream(file_path);
         std::string disk;
         std::getline(file, disk);
 
         std::vector<Block> blocks;
         auto is_free_space = false;
-        uint id = 0;
+        uint curr_id = 0;
         for (const auto& c : disk) {
             const std::size_t block_length = c - '0';
             for (std::size_t i = 0; i < block_length; ++i) {
+                const auto id = is_free_space ? 0 : curr_id;
                 const auto new_block = Block{is_free_space, id, block_length};
                 blocks.push_back(new_block);
             }
 
             if (not is_free_space) {
-                ++id;
+                ++curr_id;
             }
             is_free_space = not is_free_space;
         }
@@ -34,65 +35,65 @@ namespace {
         return blocks;
     }
 
-    [[nodiscard]] auto block_compact_disk(std::vector<Block> blocks) -> std::vector<Block> {
+    [[nodiscard]] auto block_compact_disk(std::vector<Block> disk) -> std::vector<Block> {
         const auto is_free_space = [](const Block& block){ return block.is_empty; };
-        for (auto it = blocks.rbegin(); it != blocks.rend(); ++it) {
-            if (it->is_empty) {
+        for (auto block_it = disk.rbegin(); block_it != disk.rend(); ++block_it) {
+            if (block_it->is_empty) {
                 continue;
             }
-            const auto forward_it = std::next(it).base();
-            const auto first_empty_it = std::find_if(blocks.begin(), forward_it, is_free_space);
+            const auto forward_block_it = std::next(block_it).base();
+            const auto empty_it = std::find_if(disk.begin(), forward_block_it, is_free_space);
 
-            std::swap(*it, *first_empty_it);
+            std::swap(*block_it, *empty_it);
         }
 
-        return blocks;
+        return disk;
     }
 
-    [[nodiscard]] auto file_compact_disk(std::vector<Block> blocks) -> std::vector<Block> {
-        for (auto it = blocks.rbegin(); it != blocks.rend(); std::advance(it, it->length)) {
-            if (it->is_empty) {
+    [[nodiscard]] auto file_compact_disk(std::vector<Block> disk) -> std::vector<Block> {
+        for (auto file_it = disk.rbegin(); file_it != disk.rend();) {
+            const auto file_length = file_it->length;
+            if (file_length == 0) {
+                ++file_it;
+                continue;
+            }
+            if (file_it->is_empty) {
+                std::advance(file_it, file_length);
                 continue;
             }
 
-            const auto required_length = it->length;
-            if (required_length == 0) {
-                continue;
-            }
-
-            const auto forward_it = std::next(it).base();
-
-            const auto is_valid_space = [required_length](const Block& block) {
-                return block.is_empty and block.length >= required_length;
+            // Check for empty space of required length
+            const auto forward_file_it = std::next(file_it).base();
+            const auto is_valid_space = [file_length](const Block& block) {
+                return block.is_empty and block.length >= file_length;
             };
-            auto empty_it = std::find_if(blocks.begin(), forward_it, is_valid_space);
-            if (empty_it == forward_it) {
+            auto empty_it = std::find_if(disk.begin(), forward_file_it, is_valid_space);
+            if (empty_it == forward_file_it) {
+                std::advance(file_it, file_length);
                 continue;
             }
 
-            const auto free_space = empty_it->length;
-            auto block_to_move_it = it;
-            for (auto i = 0; i < required_length; ++i) {
+            // Switch blocks in file
+            const auto free_space = empty_it->length - file_length;
+            auto block_to_move_it = file_it;
+            for (auto i = 0; i < file_length; ++i, ++block_to_move_it, ++empty_it) {
                 std::swap(*block_to_move_it, *empty_it);
-
-                ++block_to_move_it;
-                ++empty_it;
             }
 
-            const std::size_t new_free_space = free_space - required_length;
-            for (auto i = 0; i < new_free_space; ++i) {
-                empty_it->length = new_free_space;
-                ++empty_it;
+            for (auto i = 0; i < free_space; ++i, ++empty_it) {
+                empty_it->length = free_space;
             }
+
+            std::advance(file_it, file_length);
         }
 
-        return blocks;
+        return disk;
     }
 
-    [[nodiscard]] auto calculate_checksum(const std::vector<Block>& blocks) -> unsigned long {
+    [[nodiscard]] auto calculate_checksum(const std::vector<Block>& disk) -> unsigned long {
         unsigned long checksum = 0;
-        for (std::size_t i = 0; i < blocks.size(); ++i) {
-            const auto [is_empty, id, _] = blocks[i];
+        for (std::size_t i = 0; i < disk.size(); ++i) {
+            const auto [is_empty, id, _] = disk[i];
             if (is_empty) {
                 continue;
             }
@@ -106,13 +107,13 @@ namespace {
 
 auto main() -> int {
     const auto file_path = std::string{"input.txt"};
-    const auto blocks = read_blocks_from_file(file_path);
+    const auto disk = read_disk_from_file(file_path);
 
-    const auto block_compacted_disk = block_compact_disk(blocks);
+    const auto block_compacted_disk = block_compact_disk(disk);
     const auto block_move_checksum = calculate_checksum(block_compacted_disk);
     std::cout << "Checksum by moving blocks: " << block_move_checksum << '\n';
 
-    const auto file_compacted_disk = file_compact_disk(blocks);
+    const auto file_compacted_disk = file_compact_disk(disk);
     const auto file_move_checksum = calculate_checksum(file_compacted_disk);
     std::cout << "Checksum by moving files:  " << file_move_checksum << '\n';
 
