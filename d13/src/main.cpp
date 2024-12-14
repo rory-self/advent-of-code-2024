aea4bdc01d0512d69b0efd03dc9837ac1332adcf
Rory Self <rory.self1@gmail.com>
2024-12-14 14:18:59 +1100

    Finished day 14 part 1

---
+++ d13/src/main.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <vector>
+#include <array>
+#include <cmath>
+#include <fstream>
+#include <regex>
+#include <set>
+
+namespace {
+    using Matrix2x2 = std::array<std::array<float, 2>, 2>;
+    struct GameSpecs {
+        Matrix2x2 button_specs;
+        std::array<float, 2> prize_pos;
+    };
+
+    [[nodiscard]] auto calculate_2x2_discriminant(const Matrix2x2& matrix) -> float {
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+    }
+
+    /**
+     * Solves two unknown variables using Cramer's method
+     * @param matrix - 2x2 matrix holding variable coefficients
+     * @param results - answers of each simultaneous equation
+     * @return value of the two unknown variables in a std::pair
+     */
+    [[nodiscard]] auto solve_matrix_variables(const Matrix2x2& matrix, const std::array<float, 2>& results) -> std::pair<float, float> {
+        const auto raw_discriminant = calculate_2x2_discriminant(matrix);
+
+        const auto mutated_matrix1 = Matrix2x2{{{results[0], matrix[0][1]}, {results[1], matrix[1][1]}}};
+        const auto discriminant1 = calculate_2x2_discriminant(mutated_matrix1);
+        const auto variable1 = discriminant1 / raw_discriminant;
+
+        const auto mutated_matrix2 = Matrix2x2{{{matrix[0][0], results[0]}, {matrix[1][0], results[1]}}};
+        const auto discriminant2 = calculate_2x2_discriminant(mutated_matrix2);
+        const auto variable2 = discriminant2 / raw_discriminant;
+
+        return {variable1, variable2};
+    }
+
+    [[nodiscard]] auto extract_positional_floats_from_file_line(
+        std::ifstream& file,
+        const std::regex& pattern
+    ) -> std::pair<float, float> {
+        std::string haystack;
+        std::getline(file, haystack);
+
+        auto it = std::regex_iterator(haystack.cbegin(), haystack.cend(), pattern);
+        const auto match1_str = it->str();
+
+        ++it;
+        const auto match2_str = it->str();
+
+        const auto float1 = std::stof(match1_str);
+        const auto float2 = std::stof(match2_str);
+        return {float1, float2};
+    }
+
+    [[nodiscard]] auto read_crane_games_from_file(const std::string& file_path) -> std::vector<GameSpecs> {
+        std::vector<GameSpecs> all_game_specs;
+
+        const auto numeric_pattern = std::regex{"[0-9]+"};
+
+        auto file = std::ifstream(file_path);
+        std::string buffer;
+        do {
+            const auto [button_a_x, button_a_y] = extract_positional_floats_from_file_line(file, numeric_pattern);
+            const auto [button_b_x, button_b_y] = extract_positional_floats_from_file_line(file, numeric_pattern);
+            const auto [prize_x, prize_y] = extract_positional_floats_from_file_line(file, numeric_pattern);
+
+            const auto button_specs = Matrix2x2{{{button_a_x, button_b_x}, {button_a_y, button_b_y}}};
+            const auto prize_pos = std::array<float, 2>{{prize_x, prize_y}};
+            all_game_specs.push_back({button_specs, prize_pos});
+
+            std::getline(file, buffer);
+        } while (not file.eof());
+
+        return all_game_specs;
+    }
+}
+
+auto main() -> int {
+    const auto file_path = std::string{"input.txt"};
+    const auto all_game_specs = read_crane_games_from_file(file_path);
+
+    const auto is_impossible_to_press = [](const float& num_presses) {
+        return num_presses != std::floorf(num_presses) or num_presses < 0;
+    };
+
+    unsigned int tokens_required = 0;
+    for (const auto&[button_specs, prize_pos] : all_game_specs) {
+        const auto [a_presses, b_presses] = solve_matrix_variables(button_specs, prize_pos);
+        if (is_impossible_to_press(a_presses) or is_impossible_to_press(b_presses)) {
+            continue;
+        }
+
+        tokens_required += static_cast<unsigned int>(a_presses) * 3 + static_cast<int>(b_presses);
+    }
+
+    std::cout << tokens_required << std::endl;
+    return 0;
+}
