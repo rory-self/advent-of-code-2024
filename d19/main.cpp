b33a61feeb37f8af485a622febdfc876c6170936
Rory Self <rory.self1@gmail.com>
2025-03-24 13:16:16 +1100

    Refactoring

--- d19/main.cpp
+++ d19/main.cpp
@@ -1,21 +1,19 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
-#include <climits>
 #include <iostream>
 #include <sstream>
-#include <unordered_set>
 #include <unordered_map>
 
 namespace {
     using Patterns = std::vector<std::string>;
 
     [[nodiscard]] auto towel_details_from_file(
-        const std::string& file_path
+        const std::string &file_path
     ) -> std::pair<Patterns, Patterns> {
         auto file = std::ifstream(file_path);
 
-        // Collect available designs
+        // Collect available patterns
         Patterns available_patterns;
         std::string pattern_line;
         std::getline(file, pattern_line);
@@ -32,9 +30,9 @@ namespace {
 
             available_patterns.push_back(pattern);
         }
-        std::ranges::sort(available_patterns, [](const auto& p1, const auto& p2) { return p1.size() > p2.size(); });
+        std::ranges::sort(available_patterns, [](const auto &p1, const auto &p2) { return p1.size() > p2.size(); });
 
-        // Collect wanted designs
+        // Collect target designs
         std::vector<std::string> wanted_designs;
         for (std::string design; std::getline(file, design);) {
             if (design.empty()) {
@@ -47,16 +45,17 @@ namespace {
     }
 
     [[nodiscard]] auto is_possible_design(
-        const std::string& target_design,
-        const std::vector<std::string>& available_patterns,
-        std::unordered_set<std::string>& possible_designs,
-        std::unordered_set<std::string>& impossible_designs
+        const std::string &target_design,
+        const std::vector<std::string>::const_iterator &patterns_start,
+        const std::vector<std::string>::const_iterator &patterns_end,
+        std::unordered_map<std::string, bool> &design_cache
     ) -> bool {
-        const auto is_best_pattern = [&](const auto& pattern) {
-            if (pattern.size() > target_design.size()) {
-                return false;
-            }
+        const auto first_eligible_pattern = std::find_if(patterns_start, patterns_end, [target_design](const auto &p) {
+            return p.size() <= target_design.size();
+        });
 
+        const auto is_best_pattern = [&target_design, first_eligible_pattern, patterns_end, &design_cache
+                ](const auto &pattern) {
             const auto substr_start = target_design.find(pattern);
             if (substr_start == std::string::npos) {
                 return false;
@@ -71,48 +70,38 @@ namespace {
 
             if (is_start) {
                 const auto right_substr = target_design.substr(substr_end);
-                return is_possible_design(right_substr, available_patterns, possible_designs, impossible_designs);
+                return is_possible_design(right_substr, first_eligible_pattern, patterns_end, design_cache);
             }
             if (is_end) {
                 const auto left_substr = target_design.substr(0, substr_start);
-                return is_possible_design(left_substr, available_patterns, possible_designs, impossible_designs);
+                return is_possible_design(left_substr, first_eligible_pattern, patterns_end, design_cache);
             }
 
             const auto left_substr = target_design.substr(0, substr_start);
             const auto right_substr = target_design.substr(substr_end);
-            return is_possible_design(left_substr, available_patterns, possible_designs, impossible_designs)
-                and is_possible_design(right_substr, available_patterns, possible_designs, impossible_designs);
+            return is_possible_design(left_substr, first_eligible_pattern, patterns_end, design_cache)
+                   and is_possible_design(right_substr, first_eligible_pattern, patterns_end, design_cache);
         };
 
         // Cache explored design possibilities for speedup
-        if (possible_designs.contains(target_design)) {
-            return true;
-        }
-        if (impossible_designs.contains(target_design)) {
-            return false;
+        if (design_cache.contains(target_design)) {
+            return design_cache[target_design];
         }
 
-        if (std::ranges::any_of(available_patterns, is_best_pattern)) {
-            possible_designs.emplace(target_design);
-            return true;
-        }
-        impossible_designs.emplace(target_design);
-        return false;
+        const auto is_possible = std::any_of(first_eligible_pattern, patterns_end, is_best_pattern);
+        design_cache[target_design] = is_possible;
+        return is_possible;
     }
 }
 
 auto main() -> int {
     const auto file_path = std::string{"input.txt"};
-    const auto [available_designs, wanted_designs] = towel_details_from_file(file_path);
+    const auto [patterns, designs] = towel_details_from_file(file_path);
 
-    uint num_possible_designs = 0;
-    std::unordered_set<std::string> possible_designs;
-    std::unordered_set<std::string> impossible_designs;
-    for (const auto& target_design : wanted_designs) {
-        if (is_possible_design(target_design, available_designs, possible_designs, impossible_designs)) {
-            ++num_possible_designs;
-        }
-    }
+    std::unordered_map<std::string, bool> design_cache;
+    const auto num_possible_designs = std::ranges::count_if(designs, [&patterns, &design_cache](const auto &d) {
+        return is_possible_design(d, patterns.cbegin(), patterns.cend(), design_cache);
+    });
     std::cout << num_possible_designs << " designs are possible\n";
 
     return 0;
